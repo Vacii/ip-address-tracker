5fca7324c8d4f58eabb18fe5d7f9c17905009bb1
Luke <lv.vaclavek@gmail.com>
2023-09-01 12:53:59 +0200

    main function finished 🚀

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect, useMemo } from "react";
 import "./App.css";
 import "./index.css";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import Delayed from "./Delayed";
+
 function App() {
   const [ipAddress, setIPAddress] = useState("");
   const [userData, setUserData] = useState({
@@ -12,7 +14,7 @@ function App() {
       route: "104.28.114.0/24",
       type: "Content",
     },
-    ip: "",
+    ip: ipAddress,
     isp: "Cloudflare, Inc.",
     location: {
       city: "Nové Město",
@@ -42,26 +44,36 @@ function App() {
 
   const handleSearch = (e) => {
     e.preventDefault();
-    // fetch(
-    //   `https://geo.ipify.org/api/v2/country,city?apiKey=${geoAPIkey}&ipAddress=${ipAddress}`
-    // )
-    //   .then((response) => response.json())
-    //   .then((data) => setUserData(data))
-    //   .catch((error) => console.log(error));
+    fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${geoAPIkey}&ipAddress=${ipAddress}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setUserData(data);
+      })
+      .catch((error) => console.log(error));
 
-    console.log(userData);
+    // console.log(ipAddress);
   };
 
   useEffect(() => {
+    // Fetch the IP address and set it in the state
     fetch("https://api.ipify.org?format=json")
       .then((response) => response.json())
-      .then((data) => setIPAddress(data.ip))
+      .then((data) => {
+        setIPAddress(data.ip);
+        setUserData((prevUserData) => ({
+          ...prevUserData,
+          ip: data.ip,
+        }));
+      })
       .catch((error) => console.log(error));
   }, []);
 
   return (
     <>
-      <header className="h-[40%] max-h-[400px] flex flex-col items-center justify-center bg-[url(./assets/pattern-bg-mobile.png)] md:bg-[url(./assets/pattern-bg-desktop.png)] bg-no-repeat bg-cover gap-10">
+      <header className="h-[40%] max-h-[400px] flex flex-col items-center justify-center bg-[url(./assets/pattern-bg-mobile.png)] md:bg-[url(./assets/pattern-bg-desktop.png)] bg-no-repeat pt-5 md:pt-0 bg-cover gap-10">
         <h1 className="text-3xl font-medium text-white">IP Address Tracker</h1>
         <form
           className="w-[80%] max-w-[600px] flex flex-row "
@@ -71,6 +83,7 @@ function App() {
             type="text"
             name="ip-address"
             placeholder={ipAddress}
+            onChange={(event) => setIPAddress(event.target.value)}
             className="w-full p-2 px-5 h-16 text-xl rounded-l-xl font-normal cursor-pointer"
           />
           <button className="bg-black hover:bg-[#2b2b2b] w-16 flex items-center justify-center rounded-r-xl">
@@ -87,7 +100,7 @@ function App() {
             <p className="text-xs text-[#969696] uppercase tracking-wider md:tracking-widest vertical-scaling">
               IP ADDRESS
             </p>
-            <p className="text-xl md:text-2xl">{ipAddress}</p>
+            <p className="text-xl md:text-2xl">{userData.ip}</p>
           </div>
 
           <div className="flex flex-col flex-item justify-center md:justify-start items-center md:items-start userData gap-1 md:gap-3 px-5 md:h-full">
@@ -118,7 +131,7 @@ function App() {
         </div>
       </header>
       <main className="h-full w-full bg-slate-800">
-        {isLoaded ? (
+        {isLoaded && userData.location.lat && userData.location.lng ? (
           <GoogleMap
             zoom={12}
             center={{
@@ -128,16 +141,22 @@ function App() {
             mapContainerClassName="w-full h-full"
             options={defaultMapOptions}
           >
-            <Marker
-              key="marker"
-              position={{
-                lat: userData.location.lat,
-                lng: userData.location.lng,
-              }}
-            />
+            <Delayed>
+              <Marker
+                key="marker"
+                icon={"src/assets/icon-location.svg"}
+                position={{
+                  lat: userData.location.lat,
+                  lng: userData.location.lng,
+                }}
+                className="z-[100]"
+              />
+            </Delayed>
           </GoogleMap>
         ) : (
-          "Loading..."
+          <div className="loading-spinner">
+            <div className="spinner"></div>
+          </div>
         )}
       </main>
     </>
